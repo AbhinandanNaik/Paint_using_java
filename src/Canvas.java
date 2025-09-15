a79fcfc730e2637d40e335c62690e05800d5275b
Abhinandan Naik <147435815+AbhinandanNaik@users.noreply.github.com>
2025-09-15 21:43:30 +0530

    Update Canvas.java

--- src/Canvas.java
+++ src/Canvas.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
   
 public class Canvas extends JPanel {   
